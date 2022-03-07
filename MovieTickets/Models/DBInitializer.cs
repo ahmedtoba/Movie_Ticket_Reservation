db0688ede084f37958d71d474414ec0903ae2085
Your Name <you@example.com>
2022-03-07 23:45:15 +0200

    update

--- MovieTickets/Models/DBInitializer.cs
+++ MovieTickets/Models/DBInitializer.cs
@@ -134,8 +134,10 @@ namespace MovieTickets.Models
 
 
                     //Adding Movies Here
+
                     if (!db.Movies.Any())
                     {
+                    //start of adding movie
                         Guid guid1 = Guid.NewGuid();
                         var movie1 = new Movie()
                         {
@@ -152,8 +154,8 @@ namespace MovieTickets.Models
                             Image = await ImageConverter("movie-1.jpg"),
                         };
                         db.Movies.Add(movie1);
-                    var actors = new List<int> { 1, 3, 5 };
-                        foreach (var id in actors)
+                    var actors1 = new List<int> { 1, 3, 5 };
+                        foreach (var id in actors1)
                         {
                             db.MovieActors.Add(new MovieActor()
                             {
@@ -161,25 +163,149 @@ namespace MovieTickets.Models
                                 ActorId = id
                             });
                         }
-                    var cinemas = new List<int> { 1, 3, 4 };
-                    var quantities = new List<int> { 50, 40, 30 };
+                    var cinemas1 = new List<int> { 1, 3, 4 };
+                    var quantities1 = new List<int> { 50, 40, 30 };
 
                         //adding to cinema movies table
-                        for (var i = 0; i < cinemas.Count; i++)
+                        for (var i = 0; i < cinemas1.Count; i++)
                         {
                             db.MovieInCinemas.Add(new MovieInCinema()
                             {
-                                Quantity = quantities[i],
+                                Quantity = quantities1[i],
                                 MovieId = guid1,
-                                CinemaId = cinemas[i]
+                                CinemaId = cinemas1[i]
                             });
                         }
-                    
+                        //end of adding one movie
+                        
+                        //start of adding movie
+                        Guid guid2 = Guid.NewGuid();
+                        var movie2 = new Movie()
+                        {
+                            Id = guid2,
+                            Name = "Tyson's Run (2022)",
+                            StartDate = new DateTime(2022, 3, 30),
+                            EndDate = new DateTime(2022, 8, 14),
+                            Rate = 7,
+                            Trailer= "https://www.youtube.com/watch?v=hEqSj5esw7k",
+                            Price = 150,
+                            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                            Producer_Id = 1,
+                            Cat_Id = 5,
+                            Image = await ImageConverter("movie-2.jpg"),
+                        };
+                        db.Movies.Add(movie2);
+                    var actors2 = new List<int> { 2, 4, 5 };
+                        foreach (var id in actors2)
+                        {
+                            db.MovieActors.Add(new MovieActor()
+                            {
+                                MovieId = guid2,
+                                ActorId = id
+                            });
+                        }
+                    var cinemas2 = new List<int> { 2, 4 };
+                    var quantities2 = new List<int> { 40, 60 };
+
+                        //adding to cinema movies table
+                        for (var i = 0; i < cinemas2.Count; i++)
+                        {
+                            db.MovieInCinemas.Add(new MovieInCinema()
+                            {
+                                Quantity = quantities2[i],
+                                MovieId = guid2,
+                                CinemaId = cinemas2[i]
+                            });
+                        }
+                    //end of adding one movie
+
+                    //start of adding movie
+                    Guid guid3 = Guid.NewGuid();
+                    var movie3 = new Movie()
+                    {
+                        Id = guid3,
+                        Name = "Offseason (2021)",
+                        StartDate = new DateTime(2021, 3, 30),
+                        EndDate = new DateTime(2022, 9, 14),
+                        Rate = 6,
+                        Trailer = "https://www.youtube.com/watch?v=hEqSj5esw7k",
+                        Price = 150,
+                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                        Producer_Id = 2,
+                        Cat_Id = 5, 
+                        Image = await ImageConverter("movie-3.jpg"),
+                    };
+                    db.Movies.Add(movie3);
+                    var actors3 = new List<int> { 1, 2, 3 };
+                    foreach (var id in actors3)
+                    {
+                        db.MovieActors.Add(new MovieActor()
+                        {
+                            MovieId = guid3,
+                            ActorId = id
+                        });
                     }
+                    var cinemas3 = new List<int> { 2, 4 };
+                    var quantities3 = new List<int> { 40, 60 };
+
+                    //adding to cinema movies table
+                    for (var i = 0; i < cinemas3.Count; i++)
+                    {
+                        db.MovieInCinemas.Add(new MovieInCinema()
+                        {
+                            Quantity = quantities3[i],
+                            MovieId = guid3,
+                            CinemaId = cinemas3[i]
+                        });
+                    }
+                    //end of adding one movie
+
+                    //start of adding movie
+                    Guid guid4 = Guid.NewGuid();
+                    var movie4 = new Movie()
+                    {
+                        Id = guid4,
+                        Name = "Jujutsu Kaisen 0: The Movie (2021)",
+                        StartDate = new DateTime(2022, 5, 30),
+                        EndDate = new DateTime(2022, 10, 14),
+                        Rate = 7,
+                        Trailer = "https://www.youtube.com/watch?v=eGSL-l95VXw",
+                        Price = 170,
+                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                        Producer_Id = 2,
+                        Cat_Id = 5,
+                        Image = await ImageConverter("movie-4.jpg"),
+                    };
+                    db.Movies.Add(movie4);
+                    var actors4 = new List<int> { 1, 2, 3 };
+                    foreach (var id in actors4)
+                    {
+                        db.MovieActors.Add(new MovieActor()
+                        {
+                            MovieId = guid4,
+                            ActorId = id
+                        });
+                    }
+                    var cinemas4 = new List<int> { 2, 4 };
+                    var quantities4 = new List<int> { 40, 60 };
+
+                    //adding to cinema movies table
+                    for (var i = 0; i < cinemas4.Count; i++)
+                    {
+                        db.MovieInCinemas.Add(new MovieInCinema()
+                        {
+                            Quantity = quantities4[i],
+                            MovieId = guid4,
+                            CinemaId = cinemas4[i]
+                        });
+                    }
+                    //end of adding one movie
+
+                }
 
 
 
-                    db.SaveChanges();
+                db.SaveChanges();
                 }
 
             }
