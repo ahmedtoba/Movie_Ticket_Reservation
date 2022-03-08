c4e307b7db57395ab92d836fabb0cfaa678f10e9
Abdalla Tantawy <tant_9@yahoo.com>
2022-03-08 06:07:11 +0200

    edit cinemas user details

---
+++ MovieTickets/ViewModels/MovieCinemaViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MovieTickets.Models;
+using System.Collections.Generic;
+
+namespace MovieTickets.ViewModels
+{
+    public class MovieCinemaViewModel
+    {
+        public virtual Cinema Cinema { get; set; }
+        public virtual List<MovieInCinema> Movies { get; set; }
+    }
+}
